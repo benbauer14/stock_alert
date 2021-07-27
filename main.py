4965ac90451ce3a721b3a233f3179115407c3517
Ben Bauer <ben.bauer1@gmail.com>
2021-07-27 14:07:08 -0500

    previous day close info found and evaluated

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+import requests
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def stockPrice(stockTick):
+    stockParams = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": stockTick,
+        "apikey": "RDIL6MF3KVT8KLT5"
+    }
+    data = requests.get("https://www.alphavantage.co/query", params=stockParams)
+    date_keys = data.json()["Time Series (Daily)"].keys()
+    list = []
+    for key in date_keys:
+        list.append(key)
+    yesterday = list[0]
+    day_before_yest = list[1]
+    yesterday_close = data.json()["Time Series (Daily)"][yesterday]["4. close"]
+    day_before_yest_close = data.json()["Time Series (Daily)"][day_before_yest]["4. close"]
+    print(yesterday_close)
+    print(day_before_yest_close)
+
+    #set arbitary 10% threshold
+    if abs((yesterday_close - day_before_yest_close) / day_before_yest_close) >= 0.1:
+        getNews()
+stockPrice("IBM")
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+def getNews(stockTick):
+    
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
